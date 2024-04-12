65d8fa4c86c18c5d4fee6379e884b82eac366561
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-12 13:31:48 -0500

    fixed errors

--- odin-app/tailwind.config.ts
+++ odin-app/tailwind.config.ts
@@ -1,29 +1,38 @@
+/** @type {import('tailwindcss').Config}*/
 
-import { join } from 'path';
-import type { Config } from 'tailwindcss';
-
-// 1. Import the Skeleton plugin
-import { skeleton } from '@skeletonlabs/tw-plugin';
-
-const config = {
-	// 2. Opt for dark mode to be handled via the class method
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
-	],
-	theme: {
-		extend: {},
-	},
+  import { join } from 'path';
+  import type { Config } from 'tailwindcss';
+  
+  // 1. Import the Skeleton plugin
+  import { skeleton } from '@skeletonlabs/tw-plugin';
+  
+  const config = {
+	  // 2. Opt for dark mode to be handled via the class method
+	  darkMode: 'class',
+	  content: [
+		  './src/**/*.{html,js,svelte,ts}',
+		  // 3. Append the path to the Skeleton package
+		  join(require.resolve(
+			  '@skeletonlabs/skeleton'),
+			  '../**/*.{html,js,svelte,ts}'
+		  )
+	  ],
+	  theme: {
+		  extend: {},
+	  },
+	
 	plugins: [
-		// 4. Append the Skeleton plugin (after other plugins)
-		skeleton
+		skeleton({
+			themes: { preset: [ "skeleton" ] }
+		})
 	]
-} satisfies Config;
 
-export default config;
-						
+
+  } satisfies Config;
+  
+  export default config;
+						  
+
+
+
+module.exports = config;
