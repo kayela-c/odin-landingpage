8c81d6149307c9c452bd0744a43b5768bee51fe2
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-14 13:29:28 -0500

    components updated

---
+++ odin-app/src/components/Footer.svelte
@@ -0,0 +1,16 @@
+
+  <!-- Call to Action Section -->
+  <section class=" flex gap-20 py-5">
+	<div class="blue-container flex">
+	  <div class="cta-container margin-x">
+		<h3 class="heading-3">Call to action! It's time!</h3>
+		<p class="secondary-txt">
+		  Sign up for our product by clicking that button right over there!
+		</p>
+	  </div>
+	  <div class="button-div margin-x">
+		<button class="button btn-outline">Sign Up</button>
+	  </div>
+	</div>
+  </section>
+  <!-- /Call to Action Section -->
