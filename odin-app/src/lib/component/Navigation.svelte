e7badb52a63c70af8cc795c940842e7f2e3ae5bd
Kayela Claybon <34044644+kayela-c@users.noreply.github.com>
2024-04-15 23:11:30 -0500

    deleting app reverting to vanilla installation

--- odin-app/src/lib/component/Navigation.svelte
+++
@@ -1,7 +0,0 @@
-<nav class="list-nav p-4">
-    <ul class="">    
-          <li><a href="/" on:click={drawerClose}>(Home)</a></li>
-          <li><a href="/?test=2" on:click={drawerClose}>About Us</a></li>
-          <li><a href="/?test=3" on:click={drawerClose}>Call Now</a></li>
-    </ul>
-  </nav>
